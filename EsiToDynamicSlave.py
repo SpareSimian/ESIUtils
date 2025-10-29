f3235b8bacc1d8e5d6ee6f4f114dd9ab678b7008
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2025-10-28 19:44:45 -0700

    Output the PDOs as ST structs.

--- EsiToDynamicSlave.py
+++ EsiToDynamicSlave.py
@@ -1,9 +1,37 @@
 # Code generator for EtherCAT master.
 # From ESI file, generate structured text code to initialize a slave.
 
+import io
+import re
+
+stTypesToPrefix = {
+    'BOOL' : 'x',
+    'SINT' : 'si',
+    'INT' : 'i',
+    'DINT' : 'di',
+    'LINT' : 'li',
+    'USINT' : 'usi',
+    'BYTE' : 'by',
+    'UINT' : 'ui',
+    'WORD' : 'w',
+    'UDINT' : 'udi',
+    'DWORD' : 'dw',
+    'ULINT' : 'uli',
+    'LWORD' : 'lw',
+    'REAL' : 'r',
+    'LREAL' : 'lr',
+}
+
+def stTypeToPrefix(dataType):
+    if 'ARRAY ' == dataType[0:6]:
+        # prefix is 'a' followed by the prefix of the array element
+        return 'a' + stTypesToPrefix[dataType[dataType.rfind(' ') + 1:]]
+    else:
+        return stTypesToPrefix[dataType]
+
 # hex constants in ESI files look like #x0123. Remove the 0x and replace with ST's 16# prefix. 
 def numstring(xmltext):
-    if '#x' == xmltext[0:1]:
+    if '#x' == xmltext[0:2]:
         return '16#' + xmltext[2:]
     else:
         return xmltext # it's decimal
@@ -28,6 +56,37 @@ def xmlbool(xmltext):
     else:
         return ValueError("unrecognized bool value " + xmltext)
 
+def cleanName(name):
+    return re.sub(r'[ -]', '_', name)
+
+def makeSymbol(text, dataType):
+    # strip spaces and add hungarian prefix
+    return stTypeToPrefix(dataType) + re.sub(r'[ ]', '', text)
+
+def pdoToStruct(device, deviceName, which, output):
+    xml = device.find(which)
+    index = numstring(xml.find('Index').text)
+    name = xml.find('Name').text
+    structName = cleanName(deviceName) + '_' + which
+    print(f"// {index} {name}", file=output)
+    print("{attribute 'pack_mode' := '1'}", file=output)
+    print(f"TYPE {structName} :", file=output)
+    print("STRUCT", file=output)
+    # now enumerate members
+    for entry in xml.iter('Entry'):
+        dataType = entry.find('DataType').text
+        member = makeSymbol(entry.find('Name').text, dataType)
+        indexEntry = numstring(entry.find('Index').text)
+        subindexEntry = numstring(entry.find('SubIndex').text)
+        if '0' == subindexEntry:
+            subindexEntry = ''
+        else:
+            subindexEntry = ':' + subindexEntry
+        print(f'\t{member} : {dataType}; // {indexEntry}{subindexEntry}', file=output)
+    print("END_STRUCT", file=output)
+    print("END_TYPE", file=output)
+    print('\n', file=output)
+
 import argparse
 parser = argparse.ArgumentParser(description='Code generator for EtherCAT master. From ESI file, generate structured text code to initialize a slave.')
 parser.add_argument('input_filename', help='path of ESI file')
@@ -44,6 +103,8 @@ root = tree.getroot()
 vendor = root.find('Vendor')
 id = numstring(vendor.find('Id').text)
 name = vendor.find('Name').text
+
+structsString = io.StringIO() # to store struct declarations for the end
 
 stFile = open(args.output_filename, 'w')
 print('CASE readeeprom.dwVendorID OF', file=stFile)
@@ -85,9 +146,12 @@ for device in devices.iter('Device'):
             else:
                 access = '2' # write
             print(f'\t\t\t\tpSlave^.AddFMMU(dwGlobalStartAddress := 0, wLength := {lengthBytes}, usiStartBit := 0, usiEndBit := 7, wPhyssStartAddress := {startAddress}, usiPhysStartBit := 0, usiAccess := {access}, dwFlags := 1)', file=stFile)
-    
-    rxPdo = device.find('RxPdo')
-    txPdo = device.find('TxPdo')
+
+    pdoToStruct(device, name, 'RxPdo', structsString)
+    pdoToStruct(device, name, 'TxPdo', structsString)
 
 print('\t\tEND_CASE', file=stFile)
 print('END_CASE', file=stFile)
+print('\n', file=stFile)
+
+print(structsString.getvalue(), file=stFile)
