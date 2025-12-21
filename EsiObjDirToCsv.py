ab6b0f5a6ea69e111e8f65b1898bca93ec4ffc9c
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2025-12-20 19:37:13 -0800

    Factor ESI object dictionary parsing into new module esi_file.

--- EsiObjDirToCsv.py
+++ EsiObjDirToCsv.py
@@ -1,163 +1,18 @@
 # dump the object directory from an EtherCAT ESI file to a CSV table
 
 import argparse
+from esi_file import ObjectDictionary
+
 parser = argparse.ArgumentParser(description='Extract object directory from EtherCAT ESI file as table in CSV format')
 parser.add_argument('input_filename', help='path of ESI file')
 parser.add_argument('output_filename', help='path of CSV file')
 args = parser.parse_args()
 
-# Step 1: Read and parse the XML file
-import xml.etree.ElementTree as ET
-tree = ET.parse(args.input_filename)
-root = tree.getroot()
+obj_dict = ObjectDictionary.from_file(args.input_filename)
 
-# Step 2: Extract the required information
-
-# collect all potential tags to act as CSV field names
-tag_list = list()
-tag_set = set() # for testing if we have this one yet
-
-def add_tag(newTag):
-    if newTag not in tag_set:
-        tag_list.append(newTag)
-        tag_set.add(newTag)
-        #print("add_tag(" + newTag + ")")
-
-# add the tags we want up front
-add_tag('Index')
-add_tag('SubIdx')
-add_tag('Name')
-add_tag('Type')
-add_tag('BitSize')
-add_tag('BitOffs')
-add_tag('DefaultValue')
-add_tag('MinValue')
-add_tag('MaxValue')
-add_tag('Access')
-add_tag('ModbusRegister')
-add_tag('PdoMapping')
-add_tag('Comment')
-
-def parse_object(object):
-    d = dict()
-    # init dummy keys for object table
-    d['Index'] = ''
-    d['SubIdx'] = ''
-    for node in object:
-        if 'Properties' == node.tag:
-            for prop in node:
-                if 'Property' == prop.tag:
-                    # should have Name and Value children
-                    propname = prop.find('Name').text
-                    propvalue = prop.find('Value').text
-                    d[propname] = propvalue
-                    add_tag(propname)
-                else:
-                    print('Properties node contains ' + node.tag + ', skipping')
-        elif ('Info' == node.tag) or ('Flags' == node.tag):
-            # add the child nodes, instead, like flag names or min/max/default
-            for subnode in node:
-                add_tag(subnode.tag)
-                d[subnode.tag] = subnode.text
-        elif 'Comment' == node.tag:
-            # multiple comments are allowed, concatenate, possibly adding a period.
-            # this tag gets quoted, and double quotes get doubled to escape them.
-            if node.tag in d:
-                if '.' != d[node.tag][-1]:
-                    d[node.tag] = d[node.tag] + '.'
-                d[node.tag] = d[node.tag] + ' ' + node.text
-            else:
-                d[node.tag] = node.text
-        elif 'Property' == node.tag:
-            # should have Name and Value children
-            propname = node.find('Name').text
-            propvalue = node.find('Value').text
-            d[propname] = propvalue
-            add_tag(propname)
-        else:
-            add_tag(node.tag)
-            d[node.tag] = node.text
-    return d
-
-def parse_subitem(datatype):
-    d = dict()
-    subitems = dict()
-    for node in datatype:
-        if 'SubItem' == node.tag:
-            subitem = parse_object(node)
-            if 'SubIdx' not in subitem:
-                print('SubItem ' + subitem['Name'] + ' lacks SubIdx node (array?)')
-                # synthesize one to sort at end
-                subitem['SubIdx'] = '99'
-            subitems[subitem['SubIdx']] = subitem
-        else:
-            d[node.tag] = node.text
-    if len(subitems) > 0:
-        d['SubItems'] = subitems
-    return d;
-
-def parse_enum(datatype_name, datatype):
-    d = dict()
-    d['Name'] = datatype_name
-    d['BaseType'] = datatype.find('BaseType').text
-    enumValues = dict()
-    for enumInfo in datatype.iter('EnumInfo'):
-        info = dict()
-        info['Text'] = enumInfo.find('Text').text
-        comment = enumInfo.find('Comment')
-        if comment:
-            info['Comment'] = comment.text
-        value = enumInfo.find('Enum').text
-        info['Value'] = value
-        enumValues[value] = info
-    d['Values'] = enumValues
-    return d
-    
-# custom objects can refer to datatypes for internal structure
-subitemtypes_dict = dict()
-enumtypes_dict = dict()
-datatypes = root.findall('.//DataTypes/DataType')
-
-for datatype in datatypes:
-    datatype_name = datatype.find('Name').text
-    if datatype.find('EnumInfo'):
-        enumtypes_dict[datatype_name] = parse_enum(datatype_name, datatype)
-    elif datatype.find('SubItem'):
-        subitemtypes_dict[datatype_name] = parse_subitem(datatype)
-    #else:
-    #   print(f'Unknown datatype {datatype_name}')
-print(len(subitemtypes_dict), 'SubItems')
-print(len(enumtypes_dict), 'Enums')
-
-# custom types should be DT followed by 4 hex digits
-import re
-m = re.compile('^DT[0-9A-F]{4}$')
-
-objects_dict = dict()
-
-def make_object_key(object):
-    return object['Index'] + ':' + object['SubIdx']
-
-# need deepcopy of subitemtypes since they get reused
-import copy
-objects = root.findall('.//Objects/Object')
-for object in objects:
-    d = parse_object(object)
-    dt = d['Type'] # datatype
-    if m.match(dt):
-        # custom type, insert its subitems from DataType table
-        for subitem in subitemtypes_dict[dt]['SubItems'].values():
-            # assume all uses have a common Index
-            newsubitem = copy.deepcopy(subitem)
-            newsubitem['Index'] = d['Index']
-            newsubitem['Name'] = d['Name'] + '/' + newsubitem['Name']
-            objects_dict[make_object_key(newsubitem)] = newsubitem
-    else:
-        objects_dict[make_object_key(d)] = d
-    # expand objects with custom types
-    
-print(len(objects_dict), 'Objects and sub-Objects')
-object_fieldnames = tag_list
+print(len(obj_dict.subitemtypes_dict), 'SubItems')
+print(len(obj_dict.enumtypes_dict), 'Enums')
+print(len(obj_dict.objects_dict), 'Objects and sub-Objects')
 
 def write_enum(name, enum, csv_file):
     print('', file=csv_file)
@@ -170,9 +25,9 @@ def write_enum(name, enum, csv_file):
 
 from csv import DictWriter
 with open(args.output_filename, 'w', newline = '', encoding='UTF-8') as csv_file:
-    writer = DictWriter(csv_file, fieldnames = object_fieldnames)
+    writer = DictWriter(csv_file, fieldnames = obj_dict.object_fieldnames)
     writer.writeheader()
-    for object in objects_dict.values():
+    for object in obj_dict.objects_dict.values():
         writer.writerow(object)
-    for name, enum in enumtypes_dict.items():
+    for name, enum in obj_dict.enumtypes_dict.items():
         write_enum(name, enum, csv_file)
---
+++ esi_file.py
@@ -0,0 +1,156 @@
+# module with common ESI handling
+
+import xml.etree.ElementTree as ET
+import re
+import copy # need deepcopy of subitemtypes since they get reused
+
+class ObjectDictionary:
+
+    @classmethod
+    def from_string(cls, s):
+        return cls(ET.fromstring(s), "")
+
+    @classmethod
+    def from_file(cls, filename):
+        tree = ET.parse(filename)
+        return cls(tree.getroot(), filename)
+        
+    def __init__(self, root, filename):
+        self.root = root
+        self.filename = filename
+        self._tag_list = list() # list of an object's field names 
+        # for testing if we have this one yet
+        self._tag_set = set()
+        # add the tags we want up front
+        self._add_tag('Index')
+        self._add_tag('SubIdx')
+        self._add_tag('Name')
+        self._add_tag('Type')
+        self._add_tag('BitSize')
+        self._add_tag('BitOffs')
+        self._add_tag('DefaultValue')
+        self._add_tag('MinValue')
+        self._add_tag('MaxValue')
+        self._add_tag('Access')
+        self._add_tag('ModbusRegister')
+        self._add_tag('PdoMapping')
+        self._add_tag('Comment')
+        # custom objects can refer to datatypes for internal structure
+        self.subitemtypes_dict = dict()
+        self.enumtypes_dict = dict()
+        self.datatypes = root.findall('.//DataTypes/DataType')
+        for datatype in self.datatypes:
+            datatype_name = datatype.find('Name').text
+            if datatype.find('EnumInfo'):
+                self.enumtypes_dict[datatype_name] = self._parse_enum(datatype_name, datatype)
+            elif datatype.find('SubItem'):
+                self.subitemtypes_dict[datatype_name] = self._parse_subitem(datatype)
+            #else:
+            #   print(f'Unknown datatype {datatype_name}')
+        self.objects_dict = dict()
+        objects = root.findall('.//Objects/Object')
+        for object in objects:
+            d = self._parse_object(object)
+            dt = d['Type'] # datatype
+            if ObjectDictionary._m.match(dt):
+                # custom type, insert its subitems from DataType table
+                for subitem in self.subitemtypes_dict[dt]['SubItems'].values():
+                    # assume all uses have a common Index
+                    newsubitem = copy.deepcopy(subitem)
+                    newsubitem['Index'] = d['Index']
+                    newsubitem['Name'] = d['Name'] + '/' + newsubitem['Name']
+                    self.objects_dict[ObjectDictionary._make_object_key(newsubitem)] = newsubitem
+            else:
+                self.objects_dict[ObjectDictionary._make_object_key(d)] = d
+            # expand objects with custom types
+            self.object_fieldnames = self._tag_list
+
+    def _add_tag(self, newTag):
+        if newTag not in self._tag_set:
+            self._tag_list.append(newTag)
+            self._tag_set.add(newTag)
+            #print("add_tag(" + newTag + ")")
+
+    def _parse_object(self, object):
+        d = dict()
+        # init dummy keys for object table
+        d['Index'] = ''
+        d['SubIdx'] = ''
+        for node in object:
+            if 'Properties' == node.tag:
+                for prop in node:
+                    if 'Property' == prop.tag:
+                        # should have Name and Value children
+                        propname = prop.find('Name').text
+                        propvalue = prop.find('Value').text
+                        d[propname] = propvalue
+                        self._add_tag(propname)
+                    else:
+                        print('Properties node contains ' + node.tag + ', skipping')
+            elif ('Info' == node.tag) or ('Flags' == node.tag):
+                # add the child nodes, instead, like flag names or min/max/default
+                for subnode in node:
+                    self._add_tag(subnode.tag)
+                    d[subnode.tag] = subnode.text
+            elif 'Comment' == node.tag:
+                # multiple comments are allowed, concatenate, possibly adding a period.
+                # this tag gets quoted, and double quotes get doubled to escape them.
+                if node.tag in d:
+                    if '.' != d[node.tag][-1]:
+                        d[node.tag] = d[node.tag] + '.'
+                    d[node.tag] = d[node.tag] + ' ' + node.text
+                else:
+                    d[node.tag] = node.text
+            elif 'Property' == node.tag:
+                # should have Name and Value children
+                propname = node.find('Name').text
+                propvalue = node.find('Value').text
+                d[propname] = propvalue
+                self._add_tag(propname)
+            else:
+                self._add_tag(node.tag)
+                d[node.tag] = node.text
+        return d
+    
+    def _parse_subitem(self, datatype):
+        d = dict()
+        subitems = dict()
+        for node in datatype:
+            if 'SubItem' == node.tag:
+                subitem = self._parse_object(node)
+                if 'SubIdx' not in subitem:
+                    print('SubItem ' + subitem['Name'] + ' lacks SubIdx node (array?)')
+                    # synthesize one to sort at end
+                    subitem['SubIdx'] = '99'
+                subitems[subitem['SubIdx']] = subitem
+            else:
+                d[node.tag] = node.text
+        if len(subitems) > 0:
+            d['SubItems'] = subitems
+        return d;
+    
+    def _parse_enum(self, datatype_name, datatype):
+        d = dict()
+        d['Name'] = datatype_name
+        d['BaseType'] = datatype.find('BaseType').text
+        enumValues = dict()
+        for enumInfo in datatype.iter('EnumInfo'):
+            info = dict()
+            info['Text'] = enumInfo.find('Text').text
+            comment = enumInfo.find('Comment')
+            if comment:
+                info['Comment'] = comment.text
+            value = enumInfo.find('Enum').text
+            info['Value'] = value
+            enumValues[value] = info
+        d['Values'] = enumValues
+        return d
+        
+    # custom types should be DT followed by 4 hex digits
+    _m = re.compile('^DT[0-9A-F]{4}$')
+
+    @staticmethod
+    def _make_object_key(object):
+        return object['Index'] + ':' + object['SubIdx']
+
+
